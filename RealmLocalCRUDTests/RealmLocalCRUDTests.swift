6f3311e9b80bea64bbed687226810983b8f1ed52
ArunIOS-ab <51868900+ArunIOS-ab@users.noreply.github.com>
2023-07-13 17:37:06 +0530

    Update RealmLocalCRUDTests.swift

--- RealmLocalCRUDTests/RealmLocalCRUDTests.swift
+++ RealmLocalCRUDTests/RealmLocalCRUDTests.swift
@@ -18,12 +18,14 @@ final class RealmLocalCRUDTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let i = 0
+        XCTAssert(i == 0, "Is Equal to zero")
     }
 
     func testPerformanceExample() throws {
