8cceb83c1a461f7399427bd99964441f85f95916
ArunIOS-ab <rab52green@gmail.com>
2023-07-14 09:07:13 +0530

    Test Changes

--- RealmLocalCRUDTests/RealmLocalCRUDTests.swift
+++ RealmLocalCRUDTests/RealmLocalCRUDTests.swift
@@ -24,7 +24,7 @@ final class RealmLocalCRUDTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        let i = 1
+        let i = 0
         XCTAssert(i == 0, "Is Equal to zero")
     }
 
